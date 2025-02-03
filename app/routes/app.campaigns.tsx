6c7aefea2680f9032e7ac8274250b0e0e660e9f1
Shah Jalal <rjshahjalal132@gmail.com>
2025-02-03 17:41:21 +0600

    created campaign modal

--- app/routes/app.campaigns.tsx
+++ app/routes/app.campaigns.tsx
@@ -8,11 +8,14 @@ import {
 } from "@shopify/polaris";
 import { useCallback, useState } from "react";
 import { tabs } from "../utils/Tabs";
+import CreateCampaignForm from "./app.createcampaignform";
 
 type Props = {};
 
 const CampaignsPage = (props: Props) => {
   const [selected, setSelected] = useState(0);
+
+  const [activate, setActivate] = useState(false);
 
   const handleTabChange = useCallback(
     (selectedTabIndex: number) => setSelected(selectedTabIndex),
@@ -31,15 +34,23 @@ const CampaignsPage = (props: Props) => {
           </InlineGrid>
         </Layout.Section>
         {/* End: Title */}
+        {/* Begin: Tabs */}
         <Layout.Section>
+          <br />
           <LegacyCard>
             <Tabs tabs={tabs} selected={selected} onSelect={handleTabChange}>
               <LegacyCard.Section title={tabs[selected].content}>
-                {tabs[selected].content}
+                {tabs[selected].component}
               </LegacyCard.Section>
             </Tabs>
           </LegacyCard>
         </Layout.Section>
+        {/* End: Tabs */}
+        {/* Begin: Create Campaign */}
+        <Layout.Section variant="oneThird">
+          <CreateCampaignForm activate={activate} setActivate={setActivate} />
+        </Layout.Section>
+        {/* End: Create Campaign */}
       </Layout>
     </Page>
   );
---
+++ app/routes/app.createcampaignform.tsx
@@ -0,0 +1,138 @@
+import type { ActionFunction } from "@remix-run/node";
+import { Form, useActionData, useSubmit } from "@remix-run/react";
+import {
+  Button,
+  Frame,
+  Layout,
+  Modal,
+  Page,
+  TextField,
+} from "@shopify/polaris";
+import { useCallback, useState } from "react";
+
+type CreateCampaignFormProps = {
+  activate: boolean;
+  setActivate: React.Dispatch<React.SetStateAction<boolean>>;
+};
+
+export const action: ActionFunction = async ({ request }) => {};
+
+const CreateCampaignForm: React.FC<CreateCampaignFormProps> = ({
+  activate,
+  setActivate,
+}) => {
+  const handleChange = useCallback(
+    () => setActivate(!activate),
+    [activate, setActivate],
+  );
+  const activator = <Button onClick={handleChange}>Create new</Button>;
+
+  const [campaignName, setCampaignName] = useState("");
+  const handleChangeCampaignName = useCallback(
+    (newValue: string) => setCampaignName(newValue),
+    [],
+  );
+
+  const [to, setTo] = useState("");
+  const handleChangeTo = useCallback((newValue: string) => setTo(newValue), []);
+
+  const [corporation, setCorporation] = useState("");
+  const handleChangeCorporation = useCallback(
+    (newValue: string) => setCorporation(newValue),
+    [],
+  );
+
+  const [from, setFrom] = useState("");
+  const handleChangeFrom = useCallback(
+    (newValue: string) => setFrom(newValue),
+    [],
+  );
+
+  const [emailSubject, setEmailSubject] = useState("");
+  const handleChangeEmailSubject = useCallback(
+    (newValue: string) => setEmailSubject(newValue),
+    [],
+  );
+
+  const [content, setContent] = useState("");
+  const handleChangeContent = useCallback(
+    (newValue: string) => setContent(newValue),
+    [],
+  );
+
+  const submit = useSubmit();
+  const actionData = useActionData<typeof submit>();
+  console.log(actionData);
+  const sendEmail = () => submit({}, { replace: true, method: "POST" });
+
+  return (
+    <Page>
+      <Frame>
+        <Modal
+          activator={activator}
+          open={activate}
+          onClose={handleChange}
+          title="Create a new Email Campaign"
+          primaryAction={{
+            content: "Send",
+            onAction: sendEmail,
+          }}
+          secondaryActions={[
+            {
+              content: "Finish Later",
+              onAction: handleChange,
+            },
+          ]}
+        >
+          <Modal.Section>
+            <Form>
+              <Layout>
+                <Layout.Section>
+                  <TextField
+                    label="Campaign Name"
+                    value={campaignName}
+                    onChange={handleChangeCampaignName}
+                    autoComplete="off"
+                  />
+                  <TextField
+                    label="To"
+                    value={to}
+                    onChange={handleChangeTo}
+                    autoComplete="off"
+                  />
+                  <TextField
+                    label="Corporation"
+                    value={corporation}
+                    onChange={handleChangeCorporation}
+                    autoComplete="off"
+                  />
+                  <TextField
+                    label="From"
+                    value={from}
+                    onChange={handleChangeFrom}
+                    autoComplete="off"
+                  />
+                  <TextField
+                    label="Email Subject"
+                    value={emailSubject}
+                    onChange={handleChangeEmailSubject}
+                    autoComplete="off"
+                  />
+                  <TextField
+                    label="Content"
+                    value={content}
+                    onChange={handleChangeContent}
+                    autoComplete="off"
+                  />
+                  <Button>Send</Button>
+                </Layout.Section>
+              </Layout>
+            </Form>
+          </Modal.Section>
+        </Modal>
+      </Frame>
+    </Page>
+  );
+};
+
+export default CreateCampaignForm;
